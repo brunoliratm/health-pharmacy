175ee2a086a96541b00750af7a9755f617ee1562
BrunoMagno <bmagnoserver@gmail.com>
2024-06-03 00:11:22 -0300

    Update v1.7 removing problems(now finished) ;)

--- healthpharmacy/src/main/java/GUI/MenuCliente.java
+++ healthpharmacy/src/main/java/GUI/MenuCliente.java
@@ -1,11 +1,18 @@
 package GUI;
 
 import Entidade.Cliente;
+import Entidade.Produto;
 import Servico.ComprarProduto;
 import Servico.ExibirHistoricoCompras;
 import Servico.VizualizarProduto;
 import Tratamento.Limpeza;
 import Tratamento.tratamento;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -16,6 +23,37 @@ public class MenuCliente {
     Limpeza.limpeza();
     System.out.println("Menu do Cliente\n\n");
     Thread.sleep(1500);
+    System.out.println("Bem vindo ao menu do Cliente, " + cliente1.getNome() +"!");
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+    EntityManager em = emf.createEntityManager();
+
+    try {
+      // Recomendações do dia
+      List<Produto> recomendacoes = em.createQuery("SELECT p FROM Produto p ORDER BY RAND()", Produto.class).setMaxResults(3).getResultList();
+      System.out.println("\nRecomendações do dia: (Com base no seu histórico)");
+      for (Produto produto : recomendacoes) {
+        System.out.println(produto.getNome() + " - R$ " + String.format("%.2f", produto.getPreco()));
+      }
+
+      // 3 Produtos com os melhores preços
+      TypedQuery<Produto> queryMelhoresPrecos = em.createQuery(
+              "SELECT p FROM Produto p ORDER BY p.preco ASC", Produto.class
+      ).setMaxResults(3);
+      List<Produto> melhoresPrecos = queryMelhoresPrecos.getResultList();
+
+      System.out.println("\n3 Produtos com os melhores preços:");
+      for (Produto produto : melhoresPrecos) {
+        System.out.println(produto.getNome() + " - R$ " + String.format("%.2f", produto.getPreco()));
+      }
+    } catch (Exception e) {
+      System.out.println("\nErro ao buscar produtos: " + e.getMessage());
+    } finally {
+      em.close();
+      emf.close();
+    }
+
+
     System.out.println("[1] Comprar produto");
     System.out.println("[2] Vizualizar produtos disponiveis");
     System.out.println("[3] Historico de Compras");
